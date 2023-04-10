87b1d8c3cbefa37283588f0d622760bd487117c6
“AhmedShaon252” <estiackahmed252@gmail.com>
2023-04-10 20:58:49 +0600

    image  tag changed in index and admindashboard file

--- src/pages/api/userList.js
+++ src/pages/api/userList.js
@@ -4,19 +4,19 @@ export default function handler(req, res) {
     res.status(200).json([
       {
         id:"001",
-        name:"Apple Watch",
+        name:"Shaon",
         phone:"01628243978",
         mediaurl:"https://images.unsplash.com/photo-1624096104992-9b4fa3a279dd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXBwbGUlMjB3YXRjaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
       },
       {
         id:"002",
-        name:"MackBook Pro",
+        name:"Nishat",
         phone:"01905130857",
         mediaurl:"https://www.apple.com/v/macbook-air/o/images/overview/hero_mba_m1__mfge6fbp7t2m_large.jpg"
       },
       {
         id:"003",
-        name:"i-Pad",
+        name:"Shanto",
         phone:"01715933429",
         mediaurl:"https://images.unsplash.com/photo-1561154464-82e9adf32764?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
       }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 const Home = ({products}) => {
     const productList = products.map(product =>{
         return(
             <div className="" key={product.id}>
                 <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <img className="p-8 rounded-t-lg h-auto max-w-xs" src={product.mediaurl} alt="product image" />
+                    <Image className="p-8 rounded-t-lg h-auto max-w-xs" src={product.mediaurl} alt="product image" />
                     <div className="px-5 pb-5">
                         <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{product.name}</h5>
                         <div className="flex items-center justify-between">
